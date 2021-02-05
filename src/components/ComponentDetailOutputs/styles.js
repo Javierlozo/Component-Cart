8d0dae193a446c38b7524b10e6674ad25ec33287
corleyb2 <corleyb2@gmail.com>
2021-02-05 12:52:40 -0500

    bad copypasta style file overwritten

--- src/components/ComponentDetailOutputs/styles.js
+++ src/components/ComponentDetailOutputs/styles.js
@@ -1,13 +1,13 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-  footerContainer: {
-    height: "20vh",
-    backgroundColor: theme.palette.secondary.background,
-    color: theme.palette.primary.font,
-    display: "flex",
-    justifyContent: "center",
+  controlBox: {
   },
+  codeView: {
+  },
+  readMeView: {
+  }
 }));
 
 export default useStyles;
+
